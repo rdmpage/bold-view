140534fee4755e08877c1f7e2b6009d678bed658
Roderic Page <rdmpage@gmail.com>
2025-02-25 16:09:39 +0000

    Add labels to image gallery

--- gallery.css.inc.php
+++ gallery.css.inc.php
@@ -15,10 +15,25 @@
 .gallery li {
   height: 100px;
   flex-grow: 1;  
+  position:relative;
 }
 
 .gallery li:last-child {
   flex-grow: 10;
+}
+
+.gallery div {
+	font-size:0.8em;
+	
+	position:absolute;
+	left:0px;
+	top:0px;
+	width: 90%;
+	text-align:center;
+	
+	height:auto;
+	background-color:rgba(0, 0, 0, 0.4);
+	color:white;
 }
 
 .gallery img {
@@ -27,7 +42,7 @@
   object-fit: cover;
   vertical-align: bottom;
   
-  border-radius:0.2em;
+  /* border-radius:0.2em; */
   
   border:1px solid #CCC; 
 }	
