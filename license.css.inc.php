26663534a85a49924c4904bd9bdd2f6b775d8891
Roderic Page <rdmpage@gmail.com>
2025-02-25 16:08:51 +0000

    Ensure license image URLs use website root

--- license.css.inc.php
+++ license.css.inc.php
@@ -1,3 +1,8 @@
+<?php
+
+global $config;
+
+?>
 
 /* display licenses */
 .license  {
@@ -12,30 +17,30 @@
 }
 
 .cc {
-  background: url(images/cc-icons-svg/cc.svg);
+  background: url(<?php echo $config['web_root']; ?>images/cc-icons-svg/cc.svg);
   background-size: contain;
 }  			
 .by {
-  background: url(images/cc-icons-svg/by.svg);
+  background: url(<?php echo $config['web_root']; ?>images/cc-icons-svg/by.svg);
   background-size: contain;
 }  			
 .nc {
-  background: url(images/cc-icons-svg/nc.svg);
+  background: url(<?php echo $config['web_root']; ?>images/cc-icons-svg/nc.svg);
   background-size: contain;
 }  			
 .nd {
-  background: url(images/cc-icons-svg/nd.svg);
+  background: url(<?php echo $config['web_root']; ?>images/cc-icons-svg/nd.svg);
   background-size: contain;
 }  
 .pd {
-  background: url(images/cc-icons-svg/pd.svg);
+  background: url(<?php echo $config['web_root']; ?>images/cc-icons-svg/pd.svg);
   background-size: contain;
 } 			
 .sa {
-  background: url(images/cc-icons-svg/sa.svg);
+  background: url(<?php echo $config['web_root']; ?>images/cc-icons-svg/sa.svg);
   background-size: contain;
 } 
 .zero {
-  background: url(images/cc-icons-svg/zero.svg);
+  background: url(<?php echo $config['web_root']; ?>images/cc-icons-svg/zero.svg);
   background-size: contain;
 }  		
