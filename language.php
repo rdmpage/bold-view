42fd51122a50729f163a5e83f486a8c3dea1ab40
Roderic Page <rdmpage@gmail.com>
2025-08-05 18:20:26 +0100

    Add language.php

--- language.php
+++ language.php
@@ -7,19 +7,72 @@ require_once(__DIR__ . '/vendor/autoload.php');
 
 use Symfony\Component\Yaml\Yaml;
 
-$filename = 'locale/' . $config['lang'] . '.yml';
+$text_strings = array();
 
-$text_strings = Yaml::parseFile($filename);
+//----------------------------------------------------------------------------------------
+// get current language
+function get_language()
+{
+	global $config;
+	
+	/*
+	if (isset($_COOKIE["language"])) 
+	{ 
+		$lang = $_COOKIE["language"];
+	    $config['lang'] = $lang;
+	}
+	else
+	{
+		$lang = $config['lang'];
+		setcookie('language', $config['lang']); 
+	}
+	*/
+	
+	$lang = $config['lang'];
+	return $lang;
+}
 
+//----------------------------------------------------------------------------------------
+// Set language and update text strings to use that language
+function set_language($lang)
+{
+	global $config;
+	global $text_strings;
+
+	$config['lang'] = $lang;
+	setcookie('language', $config['lang']); 
+	
+	$text_strings = [];
+	load_language($config['lang']);
+}
+
+//----------------------------------------------------------------------------------------
+function load_language($lang)
+{
+	global $text_strings;
+
+	$filename = 'locale/' . $lang . '.yml';
+	$text_strings = Yaml::parseFile($filename);	   
+}
+
+//----------------------------------------------------------------------------------------
+// Get text for a key in the current language
 function get_text($key)
 {
 	global $config;
 	global $text_strings;
 	
+	$lang = get_language();
+	
+	if (!isset($text_strings[$lang]))
+	{
+		load_language($lang);
+	}
+	
 	$text = '';
 	$found = false;
 	
-	$root = $text_strings[$config['lang']];
+	$root = $text_strings[$lang];
 	
 	//print_r($root);
 	
